921eb4ee57c4d409ca34e4e79361bf1330df8524
Ted LeRoy <ted.leroy@protonmail.com>
2018-03-05 10:15:44 -0500

    Changes to be committed: 	modified:   ubuntu-update.sh 	Separated out update and upgrade 	Added comment about checking for autoclean or reboot messages

--- ubuntu-update.sh
+++ ubuntu-update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Simple bash script to updte Ubuntu Linux
+# Run ./ubuntu-update.sh with sudo privileges
+
+echo ""
+echo ""
 echo -e "\e[31mubuntu-update.sh Copyright (C) 2018 Ted LeRoy"
 echo -e "This program comes with ABSOLUTELY NO WARRANTY' see "
 echo -e "https://github.com/TedLeRoy/ubuntu-update.sh/blob/master/LICENSE.md"
@@ -7,26 +12,39 @@ echo -e "This is free software, and you are welcome to redistribute it"
 echo -e "under certain conditions."
 echo -e "Again, see  https://github.com/TedLeRoy/ubuntu-update.sh/blob/master/LICENSE.md"
 echo -e "for details.\e[0m"
+echo ""
 
 # The -e "\32m...\e[0m" code is to display output in color.
 
 echo ""
-echo ""
 echo -e "\e[32m#############################"
-echo -e "# Updating Operating System #"
+echo -e "#     Updating Data Base    #"
 echo -e "#############################\e[0m"
 echo ""
 echo ""
-apt update && apt upgrade -y
+apt update
 echo ""
 echo ""
 echo -e "\e[32m#############################"
-echo -e "#     Done With Upgrade     #"
+echo -e "#     Done With Update      #"
+echo -e "#############################\e[0m"
+echo ""
+echo ""
+echo -e "\e[32m##############################"
+echo -e "# Upgrading Operating System #"
+echo -e "##############################\e[0m"
+echo ""
+echo ""
+apt upgrade -y
+echo ""
+echo ""
+echo -e "\e[32m#############################"
+echo -e "#    OS Upgrade Complete    #"
 echo -e "#############################\e[0m"
 echo ""
 echo ""
 echo -e "\e[32m#############################"
-echo -e "#  Starting Dist-Upgrade    #"
+echo -e "#  Starting Full-Upgrade    #"
 echo -e "#############################\e[0m"
 echo ""
 echo ""
@@ -34,7 +52,11 @@ apt full-upgrade -y
 echo ""
 echo ""
 echo -e "\e[32m#############################"
-echo -e "#  Operating System Updated #"
+echo -e "#   Full Upgrade Complete   #"
 echo -e "#############################\e[0m"
 echo ""
 echo ""
+echo -e "\e[31m###################################################################"
+echo -e "#   Check for "reboot required" or "autoclean" related messages   #"
+echo -e "###################################################################\e[0m"
+
