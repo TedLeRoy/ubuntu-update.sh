20572f2269e8009b484f906dcfb7b1e756ba90af
Ted LeRoy <ted.leroy@protonmail.com>
2018-03-25 15:21:17 -0400

    Added features requested in Issue-1, command line options

--- ubuntu-update.sh
+++ ubuntu-update.sh
@@ -1,70 +1,118 @@
 #!/bin/bash
 
-# Simple bash script to updte Ubuntu Linux
-# Run ./ubuntu-update.sh with sudo privileges
+# bash script to update Ubuntu Linux. Written on 16.04 LTS Server
+# Written by Ted LeRoy with help from Google and the Linux community
+# Follow or contribute on GitHub here:
+# https://github.com/TedLeRoy/ubuntu-update.sh
 
-# [NOTE] The -e "\32m...\e[0m" code is to display output in color.
+# Defining Colors for text output
+red=$( tput setaf 1 );
+yellow=$( tput setaf 3 );
+green=$( tput setaf 2 );
+normal=$( tput sgr 0 );
 
-echo -e "
-\e[31mubuntu-update.sh Copyright (C) 2018 Ted LeRoy
-This program comes with ABSOLUTELY NO WARRANTY see 
+# Defining Header
+HEADER="
+ubuntu-update.sh Copyright (C) 2018 Ted LeRoy
+
+Easily update, upgrade, and clean up your Ubuntu system with this bash script.
+
+This program comes with ABSOLUTELY NO WARRANTY see
 https://github.com/TedLeRoy/ubuntu-update.sh/blob/master/LICENSE.md
 This is free software, and you are welcome to redistribute it
 under certain conditions.
-Again, see  https://github.com/TedLeRoy/ubuntu-update.sh/blob/master/LICENSE.md
-for details.\e[0m"
+See  https://github.com/TedLeRoy/ubuntu-update.sh/blob/master/LICENSE.md
+for details."
 
-echo -e "
+# Defining USAGE Variable to print usage for -h or undefined args
+USAGE="
+Usage: sudo bash ubuntu-update.sh [-ugdrh]
+       No option - Run all options (recommended)
+       -u Don't run apt-get update
+       -g Don't run apt-get upgrade -y
+       -d Don't run apt-get dist-upgrade -y
+       -r Don't run apt-get auto-remove
+       -h Display Usage and exit
+"
+
+# Evaluating Command Line args and setting case variables for later use
+while getopts ":ugdrh" OPT; do
+  case ${OPT} in
+    u ) uOff=1
+      ;;
+    g ) gOff=1
+      ;;
+    d ) dOff=1
+      ;;
+    r ) rOff=1
+      ;;
+    h ) hOn=1
+      ;;
+    \?) noOpt=1
+  esac
+done
+
+# Executing based on option selection
+if [[ -n $hOn || $noOpt ]]; then
+    echo "${red}$USAGE${normal}"
+    exit
+fi
+
+# Display HEADER
+echo "${red}$HEADER${normal}"
+
+if [[ ! -n $uOff ]]; then
+    echo -e "
 \e[32m#############################
 #     Updating Data Base    #
 #############################\e[0m
 "
-
 apt-get update | tee /tmp/update-output.txt
+fi
 
-echo -e "
+if [[ ! -n $gOff ]]; then
+    echo -e "
 \e[32m##############################
 # Upgrading Operating System #
 ##############################\e[0m
 "
-
 apt-get upgrade -y | tee -a /tmp/update-output.txt
+fi
 
-echo -e "
-\e[32m#############################
-#  Starting Full Upgrade    #
-#############################\e[0m
+if [[ ! -n $dOff ]]; then
+    echo -e "
+\e[32m############################
+#  Starting Full Upgrade   #
+############################\e[0m
 "
-
 apt-get dist-upgrade -y | tee -a /tmp/update-output.txt
-
 echo -e "
 \e[32m#############################
 #   Full Upgrade Complete   #
 #############################\e[0m
 "
-echo -e "
-\e[32m#############################
-#    Starting Autoremove    #
-#############################\e[0m
+fi
+
+if [[ ! -n $rOff ]]; then
+    echo -e "
+\e[32m############################
+#    Starting Autoremove   #
+############################\e[0m
 "
-
-apt-get autoremove | tee -a /tmp/update-output.txt
-
+apt-get auto-remove | tee -a /tmp/update-output.txt
 echo -e "
 \e[32m#############################
 #    Autoremove Complete    #
 #############################\e[0m
 "
+fi
 
 echo -e "
 \e[32m#####################################################
 #   Checking for actionable messages from install   #
 #####################################################\e[0m
 "
-
 egrep -wi --color 'warning|error|critical|reboot|restart|autoclean|autoremove' /tmp/update-output.txt | uniq
-
 echo -e "
 \e[32m#############################
 #    Cleaning temp files    #
@@ -72,7 +120,6 @@ echo -e "
 "
 
 rm /tmp/update-output.txt
-
 echo -e "
 \e[32m#############################
 #     Done with upgrade     #
